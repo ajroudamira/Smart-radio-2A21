67fac2e6a8eb467fcaaf699f82bfbb7da9a9e470
MalekEssayeh <Malek.Essayeh@esprit.tn>
2022-11-13 22:47:51 +0100

    5th commit mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,20 @@
 
 #include <QMainWindow>
 #include"crud.h"
+#include"metiers.h"
+#include<QtCharts>
+#include<QChartView>
+#include<QLineSeries>
+#include <QCloseEvent>
+#include <QString>
+#include <QUrl>
+#include <QKeySequenceEdit>
+#include <QDebug>
+#include <QAudioRecorder>
+#include <QAudioBuffer>
+#include <QMediaRecorder>
+#include <QAudioOutput>
+#include<QMediaPlayer>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -21,10 +35,56 @@ private slots:
     void on_tableau_activated(const QModelIndex &index);
     void on_pb_modif_clicked();
 
+    void on_pb_recher_clicked();
 
+    void on_pb_tri_clicked();
+
+    void on_pb_pdf_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_audio_pressed();
+
+    void on_audio_released();
+
+    void on_audio_clicked(bool checked);
+
+    bool on_audio_clicked();
+
+    void on_stop_clicked();
+
+
+    void on_play_clicked();
+    void handleStateChanged(QAudio::State newState)
+    {
+        switch (newState) {
+            case QAudio::IdleState:
+                // Finished playing (no more data)
+                audio->stop();
+                sourceFile.close();
+                delete audio;
+                break;
+
+            case QAudio::StoppedState:
+                // Stopped for other reasons
+                if (audio->error() != QAudio::NoError) {
+                    // Error handling
+                }
+                break;
+
+            default:
+                // ... other cases as appropriate
+                break;
+        }
+    }
 private:
     Ui::MainWindow *ui;
     CRUD c;
+    Metiers m;
+    QAudioRecorder *audioRecorder= new QAudioRecorder;
+    QFile sourceFile;
+    QAudioOutput* audio;
+
 
 };
 #endif // MAINWINDOW_H
